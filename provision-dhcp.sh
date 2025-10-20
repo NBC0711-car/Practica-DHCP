344da74d3d568fb44a3a96b4d21764aec0195b4c
Nicolas Baya-Casal Sansolini <nbaysan0711@ieszaidinvergeles.org>
2025-10-20 09:40:02 +0200

    updated the provision file of the server and also fixed a line in the conf

--- provision-dhcp.sh
+++ provision-dhcp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y isc-dhcp-server
-sudo systemctl restart isc-dhcp-server
+sudo apt install -y isc-dhcp-server net-tools iproute2
 sudo cp /etc/dhcp/dhclient.conf /etc/dhcp/dhclient.conf.bak
+sudo systemctl restart isc-dhcp-server
+sudo systemctl enable isc-dhcp-server
+sudo systemctl status isc-dhcp-server --no-pager
 
